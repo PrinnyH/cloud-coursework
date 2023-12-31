b0493d1d6f669d8ac78e70aab1886f81de06810d
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 17:01:36 +0000

    added close button too

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <!-- Navbar -->
     <div class="w3-top">
         <div class="w3-bar w3-black w3-card">
-            <a href="#login" class="w3-bar-item w3-button w3-padding-large">Login</a>
+            <a id="loginButton" href="#login" class="w3-bar-item w3-button w3-padding-large">Login</a>
             <a href="#signup" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
         </div>
     </div>
@@ -24,7 +24,7 @@
             ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
             adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         
-        <a href="#login" class="w3-black w3-bar-item w3-button w3-padding-large">Login</a>
+        <a id="loginButton" href="#login" class="w3-black w3-bar-item w3-button w3-padding-large">Login</a>
         <a href="#signup" class="w3-black w3-bar-item w3-button w3-padding-large">Sign Up</a>
     </div>
 
--- js/script.js
+++ js/script.js
@@ -5,8 +5,18 @@ document.getElementById('loginButton').addEventListener('click', function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             document.getElementById('loginOverlay').innerHTML = xhr.responseText;
             document.getElementById('loginOverlay').style.display = 'block';
+
+            // Set event listener for the close button
+            document.getElementById('closeLoginButton').addEventListener('click', function() {
+                document.getElementById('loginOverlay').style.display = 'none';
+            });
         }
     };
     xhr.send();
 });
 
+
+// Function to close the overlay
+function closeOverlay() {
+    document.getElementById('loginOverlay').style.display = 'none';
+}
