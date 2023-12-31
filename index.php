e448786df979f2804ab2bda9e0d507379633b3f2
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 16:18:09 +0000

    added style back

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <title>Page with Login Overlay</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <!-- Navbar -->
