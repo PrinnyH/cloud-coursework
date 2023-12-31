256ac3b6268f8cbc931399ed99b591d0411e087d
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 16:40:50 +0000

    removed some usisngs

--- index.php
+++ index.php
@@ -3,40 +3,39 @@
 <head>
     <title>Page with Login Overlay</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <!-- Navbar -->
-    <div class="w3-top">
-        <div class="w3-bar w3-black w3-card">
-            <a href="#login" class="w3-bar-item w3-button w3-padding-large">Login</a>
-            <a href="#signup" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
+    <div class="top">
+        <div class="bar black card">
+            <a href="#login" class="bar-item button padding-large">Login</a>
+            <a href="#signup" class="bar-item button padding-large">Sign Up</a>
         </div>
     </div>
 
     <!-- The Band Section -->
-    <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="main">
-        <h2 class="w3-wide">Vaults</h2>
-        <p class="w3-opacity"><i>Store Some Files</i></p>
+    <div class="container content center padding-64" style="max-width:800px" id="main">
+        <h2 class="wide">Vaults</h2>
+        <p class="opacity"><i>Store Some Files</i></p>
         <p class="">We have created a website. <br>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
             ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
             adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         
-        <a href="#login" class="w3-black w3-bar-item w3-button w3-padding-large">Login</a>
-        <a href="#signup" class="w3-black w3-bar-item w3-button w3-padding-large">Sign Up</a>
+        <a href="#login" class="black bar-item button padding-large">Login</a>
+        <a href="#signup" class="black bar-item button padding-large">Sign Up</a>
     </div>
 </body>
 
-<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
-    <p class="w3-medium">Created by <a target="_blank">Prince and Valli</a></p>
+<footer class="container padding-64 center opacity light-grey xlarge">
+    <i class="fa fa-facebook-official hover-opacity"></i>
+    <i class="fa fa-instagram hover-opacity"></i>
+    <i class="fa fa-snapchat hover-opacity"></i>
+    <i class="fa fa-pinterest-p hover-opacity"></i>
+    <i class="fa fa-twitter hover-opacity"></i>
+    <i class="fa fa-linkedin hover-opacity"></i>
+    <p class="medium">Created by Prince and Valli</p>
 </footer>
 
 </html>
