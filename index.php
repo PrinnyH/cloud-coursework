025ba735b4651fa314d539ad8e250c5e7894d1f4
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 17:03:36 +0000

    added css

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Page with Login Overlay</title>
+    <link rel"stylesheet" href="style.css">
     <!-- USING THE W3 style sheeet to make look prettier -->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
