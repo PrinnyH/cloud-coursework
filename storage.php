df7130feb7675c19aabd8deb8f23a532d6c0975e
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 19:39:54 +0000

    .

--- storage.php
+++ storage.php
@@ -18,28 +18,30 @@
     </div>
     
     <!-- Sidebar/nav bar -->
-    <nav id="sideNav"  class="w3-sidebar w3-bar-block w3-light-grey w3-collapse w3-top" style="z-index:3;width:250px;margin-top:83px">
-        <div id ="sideNavExpanded"> 
-            <div class="w3-container w3-display-container w3-padding-16">
-                <i class="hide-nav-button fa fas fa-angle-double-left w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
+    <div id="sideNav" style="width:250px;margin-top:83px" > 
+        <nav class="w3-sidebar w3-bar-block w3-light-grey w3-collapse w3-top" style="z-index:3;">
+            <div id ="sideNavExpanded"> 
+                <div class="w3-container w3-display-container w3-padding-16">
+                    <i class="hide-nav-button fa fas fa-angle-double-left w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
+                </div>
+                <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+                    <a href="#" class="w3-bar-item w3-button">My Files</a>
+                    <a href="#" class="w3-bar-item w3-button">My Shared Files</a>
+                </div>
+                <!-- <div class="w3-padding-16 w3-medium">
+                    <a href="#contact" class="w3-bar-item w3-text-black w3-button w3-padding">Contact</a> 
+                </div> -->
             </div>
-            <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-                <a href="#" class="w3-bar-item w3-button">My Files</a>
-                <a href="#" class="w3-bar-item w3-button">My Shared Files</a>
+            
+            <div id= "sideNavMin"> 
+                <div class="w3-container w3-display-container w3-padding-16">
+                    <i class="show-nav-button fa fas fa-angle-double-right w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
+                </div>
+                <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+                </div>
             </div>
-            <!-- <div class="w3-padding-16 w3-medium">
-                <a href="#contact" class="w3-bar-item w3-text-black w3-button w3-padding">Contact</a> 
-            </div> -->
-        </div>
-
-        <div id= "sideNavMin"> 
-            <div class="w3-container w3-display-container w3-padding-16">
-                <i class="show-nav-button fa fas fa-angle-double-right w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
-            </div>
-            <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-            </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 
     
     <!--Main Content  -->
