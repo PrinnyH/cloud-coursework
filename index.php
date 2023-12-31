cccaaa7cce5087b851e3d4f0f44108ea82ae77bf
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 16:14:34 +0000

    reformatted

--- index.php
+++ index.php
@@ -16,18 +16,17 @@
         </div>
     </div>
 
-<!-- The Band Section -->
-  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
-    <h2 class="w3-wide">Vaults</h2>
-    <p class="w3-opacity"><i>Store Some Files</i></p>
-    <p class="">We have created a website. <br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
-      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  </div>
-    <a href="#login" class="w3-bar-item w3-button w3-padding-large">Login</a>
-    <a href="#signup" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
-
+    <!-- The Band Section -->
+    <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="main">
+        <h2 class="w3-wide">Vaults</h2>
+        <p class="w3-opacity"><i>Store Some Files</i></p>
+        <p class="">We have created a website. <br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <a href="#login" class="w3-bar-item w3-button w3-padding-large">Login</a>
+        <a href="#signup" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
+    </div>
 </body>
 
 <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
