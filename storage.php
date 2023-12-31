e9357c9713dc3d87b23391ddec076802b2ec0b2b
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2023-12-31 19:46:03 +0000

    added jQuery

--- storage.php
+++ storage.php
@@ -6,6 +6,7 @@
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <title>Database Access</title>
 </head>
 <body>
