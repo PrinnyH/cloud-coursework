8a755f4f1854e9445251842e2a1b2aa2d3866b7d
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-01 04:30:31 +0000

    change to href

--- storage.php
+++ storage.php
@@ -7,7 +7,7 @@
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-    <link rel="stylesheet" src="style.css"> 
+    <link rel="stylesheet" href="style.css"> 
     <title>Database Access</title>
 </head>
 <body style="height:100%">
