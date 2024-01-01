81bffa738b1f96ae85b95cb6d1b1f5823efd32cb
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-01 04:13:40 +0000

    change position style

--- storage.php
+++ storage.php
@@ -18,30 +18,29 @@
         </div>
     </div>
     
+    <!-- Sidebar/nav bar -->
+    <nav class="sideNav w3-sidebar w3-bar-block w3-light-grey w3-top" style="width:250px; position:flex;">
+        <div id ="sideNavExpanded"> 
+            <div class="w3-container w3-display-container w3-padding-16">
+                <i class="hide-nav-button fa fas fa-angle-double-left w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
+            </div>
+            <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+                <a href="#" class="w3-bar-item w3-button">My Files</a>
+                <a href="#" class="w3-bar-item w3-button">My Shared Files</a>
+            </div>
+            <!-- <div class="w3-padding-16 w3-medium">
+                <a href="#contact" class="w3-bar-item w3-text-black w3-button w3-padding">Contact</a> 
+            </div> -->
+        </div>
+        <div id="sideNavMin" style="display: none;"> 
+            <div class="w3-container w3-display-container w3-padding-16">
+                <i class="show-nav-button fa fas fa-angle-double-right w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
+            </div>
+        </div>
+    </nav>
+
     <!--Main Content  -->
-    <div class="w3-main w3-content" style="max-width:2000px;margin-top:83px">
-        <!-- Sidebar/nav bar -->
-        <nav class="sideNav w3-sidebar w3-bar-block w3-light-grey w3-top" style="width:250px;">
-            <div id ="sideNavExpanded"> 
-                <div class="w3-container w3-display-container w3-padding-16">
-                    <i class="hide-nav-button fa fas fa-angle-double-left w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
-                </div>
-                <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-                    <a href="#" class="w3-bar-item w3-button">My Files</a>
-                    <a href="#" class="w3-bar-item w3-button">My Shared Files</a>
-                </div>
-                <!-- <div class="w3-padding-16 w3-medium">
-                    <a href="#contact" class="w3-bar-item w3-text-black w3-button w3-padding">Contact</a> 
-                </div> -->
-            </div>
-            <div id="sideNavMin" style="display: none;"> 
-                <div class="w3-container w3-display-container w3-padding-16">
-                    <i class="show-nav-button fa fas fa-angle-double-right w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
-                </div>
-            </div>
-        </nav>
-
-
+    <div class="w3-main w3-content" style="max-width:2000px; margin-top:83px;">
         <h1>Welcome [Add first name here]</h1>
          <div>
             <button id="Create_Test_Bucket" > Create Test Bucket </button>
