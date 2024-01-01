ee2f2bab582ba4e1a7b5134f4caeaaeefdf9d046
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-01 04:16:57 +0000

    moved stuff

--- storage.php
+++ storage.php
@@ -21,10 +21,9 @@
     
 
     <!--Main Content  -->
-    <div class="w3-main w3-content" style="max-width:2000px; margin-top:83px;">
-
+    <div style="max-width:2000px; margin-top:83px;">
         <!-- Sidebar/nav bar -->
-        <nav class="sideNav w3-bar-block w3-light-grey " style="width:250px; position:flex;">
+        <nav class="sideNav w3-bar-block w3-light-grey w3-left" style="width:250px; position:flex;">
             <div id ="sideNavExpanded"> 
                 <div class="w3-container w3-display-container w3-padding-16">
                     <i class="hide-nav-button fa fas fa-angle-double-left w3-display-topright w3-button w3-xxlarge w3-transparent"></i>
@@ -44,17 +43,18 @@
             </div>
         </nav>
 
-
-        <h1>Welcome [Add first name here]</h1>
-         <div>
-            <button id="Create_Test_Bucket" > Create Test Bucket </button>
-            <div id ="output">
-                <h5>...</h5>
-            </div>
-            <p>
-            <button id="Read_Test_Bucket"> Read Test Bucket </button>
-            <div id ="output2">
-                <h5>...</h5>
+        <div class="w3-main w3-content">
+            <h1>Welcome [Add first name here]</h1>
+            <div>
+                <button id="Create_Test_Bucket" > Create Test Bucket </button>
+                <div id ="output">
+                    <h5>...</h5>
+                </div>
+                <p>
+                <button id="Read_Test_Bucket"> Read Test Bucket </button>
+                <div id ="output2">
+                    <h5>...</h5>
+                </div>
             </div>
         </div>
 
