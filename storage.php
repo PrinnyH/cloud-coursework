6e4407841d5d815bdbedf35e825037c79fca2a9b
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-01 04:28:52 +0000

    moved stuff

--- storage.php
+++ storage.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="js/login.js"></script>
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-    <link rel="stylesheet" href="style.css"> 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" src="style.css"> 
     <title>Database Access</title>
 </head>
 <body style="height:100%">
