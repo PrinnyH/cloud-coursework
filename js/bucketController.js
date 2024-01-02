66187415b2a1b466f8fd951867821e4c5c1b5754
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-02 23:39:23 +0000

    test

--- js/bucketController.js
+++ js/bucketController.js
@@ -1,57 +1,60 @@
-function handleAddDirectory(){
-    var dirName = button.getAttribute('data-dir');
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'addDirectory.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    
-    xhr.onload = function() {
-        if (this.status == 200) {
-            if (this.responseText === 'true') {
-                loadDirectoryListing();
-            } else {
-                alert('There was a problem');
-            } 
-        }
-    };
-    
-    var params = new URLSearchParams();
-    params.append('dirName', dirName);
-    
-    xhr.send(params);
-};
-
-function handleDeleteDirectory(){
-    var dirName = button.getAttribute('data-dir');
-    console.log("Button clicked for directory: " + dirName);
-};
-
-function handleDeleteFile(){
-    var dirName = button.getAttribute('data-dir');
-    console.log("Button clicked for directory: " + dirName);
-};
-
-function handleUploadFile(){
-    var dirName = button.getAttribute('data-dir');
-    console.log("Button clicked for directory: " + dirName);
-};
-
-
-function loadDirectoryListing() {
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open('GET', 'loadDirectoryListing.php', true);
-    xhr.onload = function() {
-        if (this.status == 200) {
-            //document.getElementById('directoryListing').innerHTML = this.responseText;
-        } else {
-            console.error('Error loading directory listing');
-        }
-    };
-    
-    xhr.send();
-};
-    
 document.addEventListener('DOMContentLoaded', function() {
+
+    function handleAddDirectory(){
+        var dirName = button.getAttribute('data-dir');
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'addDirectory.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        xhr.onload = function() {
+            if (this.status == 200) {
+                if (this.responseText === 'true') {
+                    loadDirectoryListing();
+                } else {
+                    alert('There was a problem');
+                } 
+            }
+        };
+        
+        var params = new URLSearchParams();
+        params.append('dirName', dirName);
+        
+        xhr.send(params);
+    };
+
+    function handleDeleteDirectory(){
+        var dirName = button.getAttribute('data-dir');
+        console.log("Button clicked for directory: " + dirName);
+    };
+
+    function handleDeleteFile(){
+        var dirName = button.getAttribute('data-dir');
+        console.log("Button clicked for directory: " + dirName);
+    };
+
+    function handleUploadFile(){
+        var dirName = button.getAttribute('data-dir');
+        console.log("Button clicked for directory: " + dirName);
+    };
+
+
+    function loadDirectoryListing() {
+        var xhr = new XMLHttpRequest();
+        
+        xhr.open('POST', 'loadDirectoryListing.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        xhr.onload = function() {
+            if (this.status == 200) {
+                document.getElementById('directoryListing').innerHTML = this.responseText;
+            } else {
+                console.error('Error loading directory listing');
+            }
+        };
+        
+        xhr.send();
+    };
+    
     loadDirectoryListing();
 });
--- loadDirectoryListing.php
+++ loadDirectoryListing.php
@@ -1,101 +1,102 @@
 <?php
-
-session_start();
-
-function list_all_directories() {
     require 'vendor/autoload.php';
 
-    $storage = new StorageClient();
-    $bucket = $storage->bucket($_SESSION['user_bucket_id']);
+    use Google\Cloud\Storage\StorageClient;
 
-    $allDirectories = [];
-    foreach ($bucket->objects() as $object) {
-        $objectName = $object->name();
-        $pathParts = explode('/', $objectName);
+    session_start();
 
-        $fullPath = ''; // Initialize an empty string to build the full path
+    function list_all_directories() {
+        $storage = new StorageClient();
+        $bucket = $storage->bucket($_SESSION['user_bucket_id']);
 
-        foreach ($pathParts as $index => $part) {
-            // Skip empty parts
-            if (empty($part)) {
-                continue;
-            }
+        $allDirectories = [];
+        foreach ($bucket->objects() as $object) {
+            $objectName = $object->name();
+            $pathParts = explode('/', $objectName);
 
-            // Append this part to the full path
-            $fullPath .= $part;
+            $fullPath = ''; // Initialize an empty string to build the full path
 
-            // Determine if the current part is a directory
-            $isDirectory = $index < count($pathParts) - 1;
+            foreach ($pathParts as $index => $part) {
+                // Skip empty parts
+                if (empty($part)) {
+                    continue;
+                }
 
-            // Add a slash only if it's a directory
-            if ($isDirectory) {
-                $fullPath .= '/';
-            }
+                // Append this part to the full path
+                $fullPath .= $part;
 
-            // Add the current part to the array if it doesn't exist
-            if (!isset($allDirectories[$fullPath])) {
-                $allDirectories[$fullPath] = $isDirectory ? [] : null;
-            }
+                // Determine if the current part is a directory
+                $isDirectory = $index < count($pathParts) - 1;
 
-            // Move reference deeper into the array for directories
-            if ($isDirectory) {
-                $currentLevel = &$allDirectories[$fullPath];
+                // Add a slash only if it's a directory
+                if ($isDirectory) {
+                    $fullPath .= '/';
+                }
+
+                // Add the current part to the array if it doesn't exist
+                if (!isset($allDirectories[$fullPath])) {
+                    $allDirectories[$fullPath] = $isDirectory ? [] : null;
+                }
+
+                // Move reference deeper into the array for directories
+                if ($isDirectory) {
+                    $currentLevel = &$allDirectories[$fullPath];
+                }
             }
         }
+
+        return $allDirectories;
     }
 
-    return $allDirectories;
-}
 
+    function print_directories_html($directories) {
+        $html = "<ul class='list-dir'>";
 
-function print_directories_html($directories) {
-    $html = "<ul class='list-dir'>";
+        foreach ($directories as $dir => $subDirs) {
+            $dirSafe = htmlspecialchars($dir); // Escape the directory name
 
-    foreach ($directories as $dir => $subDirs) {
-        $dirSafe = htmlspecialchars($dir); // Escape the directory name
+            // Calculate the number of slashes in the path to determine indentation
+            $slashCount = substr_count($dirSafe, '/');
+            $indentation = $slashCount * 20; // 20px per slash for example
 
-        // Calculate the number of slashes in the path to determine indentation
-        $slashCount = substr_count($dirSafe, '/');
-        $indentation = $slashCount * 20; // 20px per slash for example
+            // Split the path and get the last part for display
+            $pathParts = explode('/', rtrim($dirSafe, '/'));
+            $displayName = end($pathParts);
 
-        // Split the path and get the last part for display
-        $pathParts = explode('/', rtrim($dirSafe, '/'));
-        $displayName = end($pathParts);
+            // Flex container for each list item's content with border and background
+            $html .= "<li class='list-dir-item'>";
+            $html .= "<div class='list-dir-item-container' style='padding-left:{$indentation}px;'>"; // Light blue border and background
 
-        // Flex container for each list item's content with border and background
-        $html .= "<li class='list-dir-item'>";
-        $html .= "<div class='list-dir-item-container' style='padding-left:{$indentation}px;'>"; // Light blue border and background
+            // Display only the last part of the path
+            $html .= "<span style='flex-grow: 1; white-space: nowrap;'>├─{$displayName}</span>";
 
-        // Display only the last part of the path
-        $html .= "<span style='flex-grow: 1; white-space: nowrap;'>├─{$displayName}</span>";
+            // Buttons (conditionally displayed)
+            if (substr($dirSafe, -1) === '/') {
+                $html .= "<span>
+                    <button class='list-dir-item-button' onclick='handleAddDirectory(this)' data-dir='{$dirSafe}'>+🗀</button>
+                    <button class='list-dir-item-button' onclick='handleDeleteDirectory(this)' data-dir='{$dirSafe}'>🗑</button>
+                    <button class='list-dir-item-button' onclick='handleUploadFile(this)' data-dir='{$dirSafe}'>+🖹</button>
+                </span>";
+            } else {
+                $html .= "<span>
+                    <button class='list-dir-item-button' onclick='handleDeleteFile(this)' data-dir='{$dirSafe}'>🗑</button>
+                </span>";
+            }
 
-        // Buttons (conditionally displayed)
-        if (substr($dirSafe, -1) === '/') {
-            $html .= "<span>
-                <button class='list-dir-item-button' onclick='handleAddDirectory(this)' data-dir='{$dirSafe}'>+🗀</button>
-                <button class='list-dir-item-button' onclick='handleDeleteDirectory(this)' data-dir='{$dirSafe}'>🗑</button>
-                <button class='list-dir-item-button' onclick='handleUploadFile(this)' data-dir='{$dirSafe}'>+🖹</button>
-            </span>";
-        } else {
-            $html .= "<span>
-                <button class='list-dir-item-button' onclick='handleDeleteFile(this)' data-dir='{$dirSafe}'>🗑</button>
-            </span>";
+            $html .= "</div>"; // Close flex container div
+
+            // Subdirectories
+            if (!empty($subDirs)) {
+                $html .= print_directories_html($subDirs);
+            }
+
+            $html .= '</li>';
         }
 
-        $html .= "</div>"; // Close flex container div
-
-        // Subdirectories
-        if (!empty($subDirs)) {
-            $html .= print_directories_html($subDirs);
-        }
-
-        $html .= '</li>';
+        $html .= '</ul>';
+        return $html;
     }
 
-    $html .= '</ul>';
-    return $html;
-}
-
-$directories = list_all_directories();
-echo print_directories_html($directories);
+    $directories = list_all_directories();
+    echo print_directories_html($directories);
 ?>
