36d13a6168a6b055412e340341928eef2b3b1435
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-02 23:55:13 +0000

    test

--- loadDirectoryListing.php
+++ loadDirectoryListing.php
@@ -1,5 +1,5 @@
 <?php
-    require 'vendor/autoload.php';
+    require "vendor/autoload.php";
 
     use Google\Cloud\Storage\StorageClient;
 
@@ -50,7 +50,7 @@
 
 
     function print_directories_html($directories) {
-        $html = "<ul class='list-dir'>";
+        $html = "<ul>";
 
         foreach ($directories as $dir => $subDirs) {
             $dirSafe = htmlspecialchars($dir); // Escape the directory name
@@ -64,8 +64,8 @@
             $displayName = end($pathParts);
 
             // Flex container for each list item's content with border and background
-            $html .= "<li class='list-dir-item'>";
-            $html .= "<div class='list-dir-item-container' style='padding-left:{$indentation}px;'>"; // Light blue border and background
+            $html .= "<li >";
+            $html .= "<div style='padding-left:{$indentation}px;'>"; // Light blue border and background
 
             // Display only the last part of the path
             $html .= "<span style='flex-grow: 1; white-space: nowrap;'>├─{$displayName}</span>";
@@ -73,13 +73,13 @@
             // Buttons (conditionally displayed)
             if (substr($dirSafe, -1) === '/') {
                 $html .= "<span>
-                    <button class='list-dir-item-button' onclick='handleAddDirectory(this)' data-dir='{$dirSafe}'>+🗀</button>
-                    <button class='list-dir-item-button' onclick='handleDeleteDirectory(this)' data-dir='{$dirSafe}'>🗑</button>
-                    <button class='list-dir-item-button' onclick='handleUploadFile(this)' data-dir='{$dirSafe}'>+🖹</button>
+                    <button onclick='handleAddDirectory(this)' data-dir='{$dirSafe}'>+🗀</button>
+                    <button onclick='handleDeleteDirectory(this)' data-dir='{$dirSafe}'>🗑</button>
+                    <button onclick='handleUploadFile(this)' data-dir='{$dirSafe}'>+🖹</button>
                 </span>";
             } else {
                 $html .= "<span>
-                    <button class='list-dir-item-button' onclick='handleDeleteFile(this)' data-dir='{$dirSafe}'>🗑</button>
+                    <button onclick='handleDeleteFile(this)' data-dir='{$dirSafe}'>🗑</button>
                 </span>";
             }
 
