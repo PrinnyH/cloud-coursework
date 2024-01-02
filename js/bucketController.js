a1a8dfeadfcfd53097cd36442d4aa8d8bf79a71f
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-02 22:45:23 +0000

    change code slight to load after page load

--- js/bucketController.js
+++ js/bucketController.js
@@ -1,53 +1,56 @@
-function handleAddDirectory(){
-    var dirName = button.getAttribute('data-dir');
+document.addEventListener('DOMContentLoaded', function() {
     
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'addDirectory.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    function handleAddDirectory(){
+        var dirName = button.getAttribute('data-dir');
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'addDirectory.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        xhr.onload = function() {
+            if (this.status == 200) {
+                if (this.responseText === 'true') {
+                    loadDirectoryListing();
+                } else {
+                    alert('There was a problem');
+                } 
+            }
+        };
+        
+        var params = new URLSearchParams();
+        params.append('dirName', dirName);
+        
+        xhr.send(params);
+    };
     
-    xhr.onload = function() {
-        if (this.status == 200) {
-            if (this.responseText === 'true') {
-                //reload page here
+    function handleDeleteDirectory(){
+        var dirName = button.getAttribute('data-dir');
+        console.log("Button clicked for directory: " + dirName);
+    };
+    
+    function handleDeleteFile(){
+        var dirName = button.getAttribute('data-dir');
+        console.log("Button clicked for directory: " + dirName);
+    };
+    
+    function handleUploadFile(){
+        var dirName = button.getAttribute('data-dir');
+        console.log("Button clicked for directory: " + dirName);
+    };
+    
+    
+    function loadDirectoryListing() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'loadDirectories.php', true);
+        
+        xhr.onload = function() {
+            if (this.status == 200) {
+                document.getElementById('directoryListing').innerHTML = this.responseText;
             } else {
-                alert('There was a problem');
-            } 
-        }
+                console.error('Error loading directory listing');
+            }
+        };
+        
+        xhr.send();
     };
-    
-    var params = new URLSearchParams();
-    params.append('dirName', dirName);
-    
-    xhr.send(params);
-}
-
-function handleDeleteDirectory(){
-    var dirName = button.getAttribute('data-dir');
-    console.log("Button clicked for directory: " + dirName);
-}
-
-function handleDeleteFile(){
-    var dirName = button.getAttribute('data-dir');
-    console.log("Button clicked for directory: " + dirName);
-}
-
-function handleUploadFile(){
-    var dirName = button.getAttribute('data-dir');
-    console.log("Button clicked for directory: " + dirName);
-}
-
-
-function loadDirectoryListing() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'loadDirectories.php', true);
-    
-    xhr.onload = function() {
-        if (this.status == 200) {
-            document.getElementById('directoryListing').innerHTML = this.responseText;
-        } else {
-            console.error('Error loading directory listing');
-        }
-    };
-    
-    xhr.send();
-}
+});
