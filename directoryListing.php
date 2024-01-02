74d8da0b313f154bcbcb83962b0485053746832b
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-02 20:24:05 +0000

    .

--- directoryListing.php
+++ directoryListing.php
@@ -29,7 +29,7 @@ function list_all_directories($bucketName) {
 }
 
 function print_directories_html($directories, $level = 0) {
-    $html = $level == 0 ? '<ul>' : '│ <ul style="list-style-type:none; padding-left:20px;">'; // Indent sub-lists
+    $html = $level == 0 ? '<ul>' : '<ul style="list-style-type:none; padding-left:20px;">│ '; // Indent sub-lists
 
     foreach ($directories as $dir => $subDirs) {
         // Escape the directory name to prevent XSS attacks
