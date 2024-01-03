2e7d1a4e05c4c4996ca72d78073a592446568aec
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-03 00:32:27 +0000

    added session start bug

--- addDirectory.php
+++ addDirectory.php
@@ -1,28 +1,26 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    require_once 'vendor/autoload.php';
 
-require_once 'vendor/autoload.php';
+    use Google\Cloud\Storage\StorageClient;
+    session_start();
 
-use Google\Cloud\Storage\StorageClient;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $dirName = $_POST['dirName'];
+        
+        $storage = new StorageClient();
+        $bucket = $storage->bucket($_SESSION['user_bucket_id']);
+        
+        // Specify the new folder path in the bucket
+        $newFolderPath = $dirName . 'newfolder/'; // Change 'newfolder' to your desired folder name
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $dirName = $_POST['dirName'];
-    
-    $storage = new StorageClient();
-    $bucket = $storage->bucket($_SESSION['user_bucket_id']);
-    
-    // Specify the new folder path in the bucket
-    $newFolderPath = $dirName . 'newfolder/'; // Change 'newfolder' to your desired folder name
+        // Create a pseudo-folder by uploading an empty file
+        $file = $bucket->upload('', 
+        ['name' => $newFolderPath . '.dummy'] // This creates a dummy file in the new folder
+        );
 
-    // Create a pseudo-folder by uploading an empty file
-    $file = $bucket->upload('', 
-    ['name' => $newFolderPath . '.dummy'] // This creates a dummy file in the new folder
-    );
-
-    echo 'true';
-} else {
-    echo 'false';
-}
+        echo 'true';
+    } else {
+        echo 'false';
+    }
 
 ?>
