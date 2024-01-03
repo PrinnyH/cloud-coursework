d7c56615617ea65389f7ba7405e01378eddeaa89
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-03 03:35:38 +0000

    styling

--- storage.php
+++ storage.php
@@ -59,7 +59,7 @@ $bucket_id = $_SESSION['user_bucket_id'];
             <div>
                 <ul class='list-dir'>
                     <li class='list-dir-item'>
-                        <div class='list-dir-item-container' style='padding-left:0px; justify-content:right; border: 0px solid background:lightsalmon;'>
+                        <div class='list-dir-item-container' style='padding-left:0px; justify-content:right; border: 0px solid; background:lightsalmon;'>
                             <span>
                                 <button class='list-dir-item-button' onclick='handleAddDirectory(this)' data-dir=''>+🗀</button>
                                 <button class='list-dir-item-button' onclick='handleUploadFile(this)' data-dir=''>+🖹</button>
