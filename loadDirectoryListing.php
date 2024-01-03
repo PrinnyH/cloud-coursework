13a23a96f4d881cdb218af49731f53d30813afe9
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-03 05:25:39 +0000

    fixed bug

--- loadDirectoryListing.php
+++ loadDirectoryListing.php
@@ -76,7 +76,7 @@
             $displayName = end($pathParts);
             
             // Flex container for each list item's content with border and background
-            $html .= "<li class='list-dir-item' draggable='true' ondragstart='dragStart(event)' ondragover='dragOver(event)' ondrop='drop(event)' data-dir='{$dirSafe} style='padding-left:{$indentation}px;'>";
+            $html .= "<li class='list-dir-item' draggable='true' ondragstart='dragStart(event)' ondragover='dragOver(event)' ondrop='drop(event)' data-dir='{$dirSafe}' style='padding-left:{$indentation}px;'>";
             $html .= "<div class='list-dir-item-container'>"; // Light blue border and background
     
             // Determine if it's a file and split the name and extension
