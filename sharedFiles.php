b3082fd58b3e20a5fbe103dc897aa45fa6bcc4e9
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 19:57:31 +0000

    test

--- sharedFiles.php
+++ sharedFiles.php
@@ -2,9 +2,9 @@
 <div class="dropdown" onchange="dropDownChange()">
   <button class="dropbtn">Dropdown</button>
   <div  id="sharedFolderSelector" class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
+    <button href="#">Link 1</button>
+    <button href="#">Link 2</button>
+    <button href="#">Link 3</button>
   </div>
 </div>
 <button class="dropbtn">Dropdown</button>
