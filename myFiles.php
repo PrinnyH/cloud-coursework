20af767787c96ea02b24c59dcac81273f7792226
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 07:34:56 +0000

    remoced wrong part of script

--- myFiles.php
+++ myFiles.php
@@ -22,4 +22,4 @@
 <h1>Welcome <?php echo $name;?> + <?php echo $bucket_id;?></h1>
 <div id="directoryListing">
 </div>
-<script>{loadDirectoryListing();}</script>
+<script> loadDirectoryListing(); </script>
