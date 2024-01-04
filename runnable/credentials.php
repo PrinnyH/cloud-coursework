ee3b3a692482678002926f3ab3f1a263c508043b
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 03:09:30 +0000

    updated creds

--- runnable/credentials.php
+++ runnable/credentials.php
@@ -1,5 +1,5 @@
 <?php
-    $host = "127.0.0.1";         // Localhost because Cloud SQL Proxy is running locally
+    $host = "10.107.112.3";         // Localhost because Cloud SQL Proxy is running locally
     $port = "3306";              // The local port that Cloud SQL Proxy is forwarding to
     $username = "vaultz-user";
     $password = "f}]^x\a>9Kk#D2xF"; // Your MySQL database password
