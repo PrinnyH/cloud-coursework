601b329245b13b9f3d1536629b0daf18ff225d2a
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 19:27:41 +0000

    test

--- runnable/testConnection.php
+++ runnable/testConnection.php
@@ -6,12 +6,12 @@
     // Replace the placeholder with your Atlas connection string
     $uri = 'mongodb://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority';
     // Create a new client and connect to the server
-    $client = new Client($uri);
+    $client = new Client($uri, ['readPreference' => 'secondaryPreferred',]);
     try {
         // Send a ping to confirm a successful connection
-        //$client->selectDatabase('admin')->command(['ping' => 1]);
-        $test = $client->website->Users;
-        echo "Pinged your deployment. You successfully connected to MongoDB!\n" . $test;
+        $client->website->command(['ping' => 1]);
+        //$test = $client->website->Users;
+        echo "Pinged your deployment. You successfully connected to MongoDB!\n";
     } catch (Exception $e) {
         printf($e->getMessage());
     }
