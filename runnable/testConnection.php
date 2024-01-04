3dcfc072da59dff74fd625e446ff809c91f48099
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 18:14:36 +0000

    updated

--- runnable/testConnection.php
+++ runnable/testConnection.php
@@ -1,10 +1,13 @@
 <?php
+
 use MongoDB\Client;
+use MongoDB\Driver\ServerApi;
 
-$uri = "mongodb+srv://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority";
-
+$uri = mongodb+srv://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority;
+// Specify Stable API version 1
+$apiVersion = new ServerApi(ServerApi::V1);
 // Create a new client and connect to the server
-$client = new MongoDB\Client($uri);
+$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
 try {
     // Send a ping to confirm a successful connection
     $client->selectDatabase('admin')->command(['ping' => 1]);
