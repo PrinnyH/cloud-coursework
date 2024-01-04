d850cb3add8b7594bd66d133bc2b326a860dfb73
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 06:43:54 +0000

    added logging

--- runnable/loadDirectoryListing.php
+++ runnable/loadDirectoryListing.php
@@ -1,5 +1,9 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     require_once('../vendor/autoload.php');
     use Google\Cloud\Storage\StorageClient;
     
@@ -18,7 +22,7 @@
                 $bucket_id = $decodedToken->bucket_id;
             }
         }
-        
+
         $storage = new StorageClient();
         $bucket = $storage->bucket($bucket_id);
     
