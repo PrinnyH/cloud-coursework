5f8e5374be032360212cf247d60ebb982016afc0
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 19:21:49 +0000

    remove selec database

--- runnable/testConnection.php
+++ runnable/testConnection.php
@@ -9,7 +9,7 @@
     $client = new Client($uri);
     try {
         // Send a ping to confirm a successful connection
-        $client->selectDatabase('admin')->command(['ping' => 1]);
+        //$client->selectDatabase('admin')->command(['ping' => 1]);
         echo "Pinged your deployment. You successfully connected to MongoDB!\n";
     } catch (Exception $e) {
         printf($e->getMessage());
