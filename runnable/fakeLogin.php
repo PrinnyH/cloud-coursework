c97f6b0b5b81dd7e7f15a35567e8a8032bdb5a84
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 22:36:39 +0000

    fixed url

--- runnable/fakeLogin.php
+++ runnable/fakeLogin.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('vendor/autoload.php');
+    require_once('../vendor/autoload.php');
     require_once('runnable/credentials.php');
     use Firebase\JWT\JWT;
     $updatedToken JWT::encode(['email' => 'zionmaster100@gmail.com', 'username' => 'prince', 'bucket_id' => '87ca841e86dbc89dd26f72211ff40fb947ce0e12'], $secretKey, 'HS256');
