48256ee28a76d0b03986fc1c0aaa2bab489da285
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 23:37:39 +0000

    created test php

---
+++ runnable/testDatabase.php
@@ -0,0 +1,44 @@
+<?php
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    // Replace these with your database credentials
+    $host = "10.107.112.3";
+    $port = "3306";             
+    $username = "root";
+    $password = "f}]^x\a>9Kk#D2xF"; 
+    $database = "vaultzsSite";
+    $port = 3306;              // Database port (default is 3306 for MySQL)
+
+    // Attempt to connect to the database
+    $mysqli = new mysqli($host, $username, $password, $database, $port);
+
+    // Check the connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    } else {
+        echo "Connected successfully to the database.";
+    }
+
+    if ($stmt = $mysqli->prepare("SELECT Shared_BucketBucketID FROM User_Shared_Bucket WHERE UserEmail = ?")) {
+        // Bind parameters and execute query
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Fetch the results into an array
+        while ($row = $result->fetch_assoc()) {
+            $allBucketIds[] = $row['Shared_BucketBucketID'];
+        }
+        echo $allBucketIds
+
+        $stmt->close();
+    } else {
+        error_log('Error preparing statement: ' . $mysqli->error);
+        echo("false");
+    }
+
+
+    // Close the connection
+    $mysqli->close();
+?>
