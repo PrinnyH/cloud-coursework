880863eef5972317d9fcc4a7dc3f77b0efd42d16
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 18:52:02 +0000

    updates

--- runnable/testConnection.php
+++ runnable/testConnection.php
@@ -1,14 +1,19 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-use MongoDB\Client;
-$uri = "mongodb+srv://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority";
-// Create a new client and connect to the server
-$client = new MongoDB\Client($uri);
-try {
-    // Send a ping to confirm a successful connection
-    $client->selectDatabase('admin')->command(['ping' => 1]);
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch (Exception $e) {
-    printf($e->getMessage());
-}
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    use MongoDB\Client;
+    use MongoDB\Driver\ServerApi;
+    $uri = "mongodb+srv://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority";
+    // Specify Stable API version 1
+    $apiVersion = new ServerApi(ServerApi::V1);
+    // Create a new client and connect to the server
+    $client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
+    try {
+        // Send a ping to confirm a successful connection
+        $client->selectDatabase('admin')->command(['ping' => 1]);
+        echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+    } catch (Exception $e) {
+        printf($e->getMessage());
+    }
+?>
