6b8e28e78edae67c38fe54d68da980cd3ed69a6c
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-04 19:10:22 +0000

    updated test conn

--- runnable/testConnection.php
+++ runnable/testConnection.php
@@ -2,13 +2,11 @@
 
     require_once __DIR__ . '/../vendor/autoload.php';
 
-    use MongoDB\MongoDB\Client;
-    use MongoDB\MongoDB\Driver\ServerApi;
-    $uri = "mongodb+srv://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority";
-    // Specify Stable API version 1
-    $apiVersion = new ServerApi(ServerApi::V1);
+    use MongoDB\Client;
+    // Replace the placeholder with your Atlas connection string
+    $uri = 'mongodb+srv://vaultz-user:r7z82ChXYpPkwIoX@website.7x18c79.mongodb.net/?retryWrites=true&w=majority';
     // Create a new client and connect to the server
-    $client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
+    $client = new MongoDB\Client($uri);
     try {
         // Send a ping to confirm a successful connection
         $client->selectDatabase('admin')->command(['ping' => 1]);
