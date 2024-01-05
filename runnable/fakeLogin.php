4ac08801b91dbaebb3c2a189ece7fc21dc1452b3
Prince Hussain <61016318+PrinnyH@users.noreply.github.com>
2024-01-05 00:50:04 +0000

    removed fake login button

--- runnable/fakeLogin.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('../vendor/autoload.php');
-    $secretKey = 'password123**1$$23';
-    use Firebase\JWT\JWT;
-    $updatedToken = JWT::encode(['email' => 'zionmaster100@gmail.com', 'username' => 'prince', 'bucket_id' => '87ca841e86dbc89dd26f72211ff40fb947ce0e12'], $secretKey, 'HS256');
-    echo json_encode(["success" => true, "updatedToken" => $updatedToken]);
-?>
